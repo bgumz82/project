5156c3bc5e028fd1cb614a5ab90cdce5c9d00163
Bruno Cesar Gumz <60102292+bgumz82@users.noreply.github.com>
2025-08-17 22:17:36 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://sistema.systemtruck.com.br:3000',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api')
